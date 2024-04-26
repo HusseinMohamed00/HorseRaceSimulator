a3766c1a88f9aeb25f5d0f3de6978a50ee911607
Hussein Mohamed <husseinmohamed@husseins-air.wkstn.qmul.ac.uk>
2024-04-26 16:36:47 +0100

    commit Main

---
+++ Part1/Main.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+class Main {
+    
+    public static void main(String[] args) {
+        // int distance = 0;
+        // ArrayList<Horse> horses = new ArrayList<Horse>();
+        // new GUI(distance,horses);
+        RacePart1 a = new RacePart1(20);
+        a.startRace();
+
+
+
+
+       
+
+
+
+
+        
+
+    }
+}
