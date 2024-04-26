bb1cea73e534c9efbbdd04998a13425595ee1b46
Hussein Mohamed <husseinmohamed@husseins-air.wkstn.qmul.ac.uk>
2024-04-26 19:07:43 +0100

    initialises arraylist and calls main gui which lets the user pick if they want to start the race

---
+++ Part2/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+class Main {
+    
+    public static void main(String[] args) {
+        int distance = 0;
+         ArrayList<Horse> horses = new ArrayList<Horse>();
+         new GUI(distance,horses);
+
+
+
+
+       
+
+
+
+
+        
+
+    }
+}
